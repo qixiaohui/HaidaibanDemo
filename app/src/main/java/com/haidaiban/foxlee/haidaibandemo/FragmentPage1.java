c4a3ba5e27768b112d8d797646881ffa676bc0b3
qixiaohui <qixiaohui@qixiaohuis-MacBook-Air.local>
2015-03-07 20:30:24 -0600

    first commit

---
+++ app/src/main/java/com/haidaiban/foxlee/haidaibandemo/FragmentPage1.java
@@ -0,0 +1,75 @@
+package com.haidaiban.foxlee.haidaibandemo;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+
+public class FragmentPage1 extends Fragment{
+
+    private View view;
+
+    private SliderLayout slider;
+
+    private Context context;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+        context = container.getContext();
+
+        view = inflater.inflate(R.layout.fragment_1, null);
+
+        slider = (SliderLayout)view.findViewById(R.id.slider);
+
+        HashMap<String, String> urlMap = new HashMap<String,String>();
+        urlMap.put("Haniba","http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        urlMap.put("big bang","http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        urlMap.put("house of cards","http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        urlMap.put("game of throne","http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        HashMap<String,Integer> fileMap = new HashMap<String,Integer>();
+        fileMap.put("Haniba",R.drawable.hannibal);
+        fileMap.put("bang bang",R.drawable.bigbang);
+        fileMap.put("house of cards",R.drawable.house);
+        fileMap.put("game of throne",R.drawable.game_of_thrones);
+
+        System.out.println(fileMap+"map");
+
+        for(String name : fileMap.keySet()){
+            TextSliderView sliderView = new TextSliderView(context);
+            sliderView
+                    .description(name)
+                    .image(fileMap.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+            slider.addSlider(sliderView);
+
+        }
+
+        slider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        slider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        slider.setCustomAnimation(new DescriptionAnimation());
+        slider.setDuration(4000);
+
+		return view;
+	}
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        System.out.println("on attachment"+slider);
+    }
+}
---
+++ app/src/main/java/com/haidaiban/foxlee/haidaibandemo/MainTabActivity.java
@@ -0,0 +1,67 @@
+package com.haidaiban.foxlee.haidaibandemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
+
+import com.daimajia.slider.library.SliderLayout;
+
+/**
+ * ji li 3.2.2015
+ */
+public class MainTabActivity extends FragmentActivity{	
+	private FragmentTabHost mTabHost;
+	
+
+	private LayoutInflater layoutInflater;
+		
+	// each fragment
+	private Class fragmentArray[] = {FragmentPage1.class,FragmentPage2.class,FragmentPage3.class,FragmentPage4.class,FragmentPage5.class};
+	
+	//底部菜单 图片
+	private int mImageViewArray[] = {R.drawable.main_bottom_tab_category_normal,R.drawable.tab_message_btn,R.drawable.tab_selfinfo_btn,
+									 R.drawable.tab_square_btn,R.drawable.tab_more_btn};
+	
+	private String mTextviewArray[] = {"菜单", "待发询价", "已发询价", "订单", "帮助"};
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_tab_layout);
+        
+        initView();
+    }
+	 
+
+	private void initView(){
+		layoutInflater = LayoutInflater.from(this);
+				
+		mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+		mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);	
+		
+		int count = fragmentArray.length;
+
+
+		for(int i = 0; i < count; i++){	
+			TabSpec tabSpec = mTabHost.newTabSpec(mTextviewArray[i]).setIndicator(getTabItemView(i));
+			mTabHost.addTab(tabSpec, fragmentArray[i], null);
+			mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.drawable.selector_tab_background);
+		}
+	}
+
+	private View getTabItemView(int index){
+		View view = layoutInflater.inflate(R.layout.tab_item_view, null);
+	
+		ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
+		imageView.setImageResource(mImageViewArray[index]);
+		
+		TextView textView = (TextView) view.findViewById(R.id.textview);		
+		textView.setText(mTextviewArray[index]);
+	
+		return view;
+	}
+}
