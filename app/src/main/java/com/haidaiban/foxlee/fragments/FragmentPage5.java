46ab4e5669808d982e29ef1fb8e3e9c9b89f0bfb
qixiaohui <qixiaohuihaha@gmail.com>
2015-03-08 21:06:20 -0500

    add web method

--- app/src/main/java/com/haidaiban/foxlee/fragments/FragmentPage5.java
+++ app/src/main/java/com/haidaiban/foxlee/fragments/FragmentPage5.java
@@ -1,15 +1,78 @@
 package com.haidaiban.foxlee.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import com.haidaiban.foxlee.webMethod.Webmethod;
+
+import java.io.IOException;
 
 public class FragmentPage5 extends Fragment{
 
+    private View view;
+    private Button button;
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {	
-		return inflater.inflate(R.layout.fragment_5, null);		
-	}	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        view  = inflater.inflate(R.layout.fragment_5, null);
+        button = (Button) view.findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+//                AsyncHttpClient client = new AsyncHttpClient();
+//                client.post("http://stage.sanqtech.com:7777/login/",new AsyncHttpResponseHandler() {
+//
+//                    @Override
+//                    public void onStart(){
+//                        RequestParams param = new RequestParams();
+//                        param.put("login","foxlee");
+//                        param.put("password","liji1025");
+//                    }
+//
+//                    @Override
+//                    public void onSuccess(int i, Header[] headers, byte[] bytes) {
+//                        System.out.println("success");
+//                        System.out.println(headers[0]);
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+//                        System.out.println("failed");
+//                    }
+//                });
+                loginAsync login = new loginAsync();
+                login.execute();
+            }
+        });
+		return view;
+	}
+
+
+    public class loginAsync extends AsyncTask<String,String,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            final Webmethod webMethod = new Webmethod(getActivity().getApplicationContext());
+            try {
+                webMethod.get();
+                webMethod.post();
+            }catch (IOException e){
+                System.out.println("%%%%%IOException");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
 }
---
+++ app/src/main/java/com/haidaiban/foxlee/webMethod/Webmethod.java
@@ -0,0 +1,68 @@
+package com.haidaiban.foxlee.webMethod;
+
+import android.content.Context;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.IOException;
+
+/**
+ * Created by qixiaohui on 3/8/15.
+ */
+public class Webmethod {
+    private static String url = "http://stage.sanqtech.com:7777/login";
+    private Context context;
+    private static ArrayList<NameValuePair> pair;
+    private static DefaultHttpClient httpClient;
+    private static List<Cookie> cookies;
+    private static Cookie middleWareToken;
+
+    public Webmethod(Context context) {
+        this.context = context;
+        httpClient = new DefaultHttpClient();
+    }
+
+    public static void get()throws IOException{
+        HttpGet get = new HttpGet(url);
+        httpClient.execute(get);
+        cookies = httpClient.getCookieStore().getCookies();
+    }
+
+    public static void post()throws IOException{
+
+        pair = new ArrayList<NameValuePair>();
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("csrftoken")){
+                System.out.println("find token (*************"+cookie.getValue());
+                middleWareToken = cookie;
+                break;
+            }
+        }
+        pair.add(new BasicNameValuePair("csrfmiddlewaretoken",middleWareToken.getValue()));
+        pair.add(new BasicNameValuePair("login","foxlee"));
+        pair.add(new BasicNameValuePair("password","liji1025"));
+
+
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("content-type","application/x-www-form-urlencoded");
+        httpPost.setEntity(new UrlEncodedFormEntity(pair));
+        HttpResponse httpResponse = httpClient.execute(httpPost);
+        System.out.println(httpResponse.getStatusLine() + "status******");
+        HttpEntity entity = httpResponse.getEntity();
+        String response = EntityUtils.toString(entity,"UTF-8");
+        System.out.println(response+"*************");
+
+    }
+
+}
