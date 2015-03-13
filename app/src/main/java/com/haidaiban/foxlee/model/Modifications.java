b90a6bb51359d8bcf46a7200b192d91c083ec9ea
qixiaohui <qixiaohuihaha@gmail.com>
2015-03-12 22:29:23 -0500

    generate pojo for offer json

---
+++ app/src/main/java/com/haidaiban/foxlee/model/Modifications.java
@@ -0,0 +1,297 @@
+package com.haidaiban.foxlee.model;
+
+/**
+ * Created by qixiaohui on 3/12/15.
+ */
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
+public class Modifications {
+
+    @Expose
+    private Boolean coupon;
+    @Expose
+    private Boolean weight;
+    @Expose
+    private Boolean remark;
+    @Expose
+    private Boolean image0;
+    @SerializedName("direct_ship")
+    @Expose
+    private Boolean directShip;
+    @Expose
+    private Boolean image1;
+    @Expose
+    private Boolean image2;
+    @SerializedName("web_link")
+    @Expose
+    private Boolean webLink;
+    @Expose
+    private Boolean shipping;
+    @Expose
+    private Boolean custom;
+    @Expose
+    private Boolean style;
+    @Expose
+    private Boolean title;
+    @Expose
+    private Boolean quantity;
+    @Expose
+    private Boolean price;
+
+    /**
+     *
+     * @return
+     * The coupon
+     */
+    public Boolean getCoupon() {
+        return coupon;
+    }
+
+    /**
+     *
+     * @param coupon
+     * The coupon
+     */
+    public void setCoupon(Boolean coupon) {
+        this.coupon = coupon;
+    }
+
+    /**
+     *
+     * @return
+     * The weight
+     */
+    public Boolean getWeight() {
+        return weight;
+    }
+
+    /**
+     *
+     * @param weight
+     * The weight
+     */
+    public void setWeight(Boolean weight) {
+        this.weight = weight;
+    }
+
+    /**
+     *
+     * @return
+     * The remark
+     */
+    public Boolean getRemark() {
+        return remark;
+    }
+
+    /**
+     *
+     * @param remark
+     * The remark
+     */
+    public void setRemark(Boolean remark) {
+        this.remark = remark;
+    }
+
+    /**
+     *
+     * @return
+     * The image0
+     */
+    public Boolean getImage0() {
+        return image0;
+    }
+
+    /**
+     *
+     * @param image0
+     * The image0
+     */
+    public void setImage0(Boolean image0) {
+        this.image0 = image0;
+    }
+
+    /**
+     *
+     * @return
+     * The directShip
+     */
+    public Boolean getDirectShip() {
+        return directShip;
+    }
+
+    /**
+     *
+     * @param directShip
+     * The direct_ship
+     */
+    public void setDirectShip(Boolean directShip) {
+        this.directShip = directShip;
+    }
+
+    /**
+     *
+     * @return
+     * The image1
+     */
+    public Boolean getImage1() {
+        return image1;
+    }
+
+    /**
+     *
+     * @param image1
+     * The image1
+     */
+    public void setImage1(Boolean image1) {
+        this.image1 = image1;
+    }
+
+    /**
+     *
+     * @return
+     * The image2
+     */
+    public Boolean getImage2() {
+        return image2;
+    }
+
+    /**
+     *
+     * @param image2
+     * The image2
+     */
+    public void setImage2(Boolean image2) {
+        this.image2 = image2;
+    }
+
+    /**
+     *
+     * @return
+     * The webLink
+     */
+    public Boolean getWebLink() {
+        return webLink;
+    }
+
+    /**
+     *
+     * @param webLink
+     * The web_link
+     */
+    public void setWebLink(Boolean webLink) {
+        this.webLink = webLink;
+    }
+
+    /**
+     *
+     * @return
+     * The shipping
+     */
+    public Boolean getShipping() {
+        return shipping;
+    }
+
+    /**
+     *
+     * @param shipping
+     * The shipping
+     */
+    public void setShipping(Boolean shipping) {
+        this.shipping = shipping;
+    }
+
+    /**
+     *
+     * @return
+     * The custom
+     */
+    public Boolean getCustom() {
+        return custom;
+    }
+
+    /**
+     *
+     * @param custom
+     * The custom
+     */
+    public void setCustom(Boolean custom) {
+        this.custom = custom;
+    }
+
+    /**
+     *
+     * @return
+     * The style
+     */
+    public Boolean getStyle() {
+        return style;
+    }
+
+    /**
+     *
+     * @param style
+     * The style
+     */
+    public void setStyle(Boolean style) {
+        this.style = style;
+    }
+
+    /**
+     *
+     * @return
+     * The title
+     */
+    public Boolean getTitle() {
+        return title;
+    }
+
+    /**
+     *
+     * @param title
+     * The title
+     */
+    public void setTitle(Boolean title) {
+        this.title = title;
+    }
+
+    /**
+     *
+     * @return
+     * The quantity
+     */
+    public Boolean getQuantity() {
+        return quantity;
+    }
+
+    /**
+     *
+     * @param quantity
+     * The quantity
+     */
+    public void setQuantity(Boolean quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     *
+     * @return
+     * The price
+     */
+    public Boolean getPrice() {
+        return price;
+    }
+
+    /**
+     *
+     * @param price
+     * The price
+     */
+    public void setPrice(Boolean price) {
+        this.price = price;
+    }
+
+}
---
+++ app/src/main/java/com/haidaiban/foxlee/model/TransactionState.java
@@ -0,0 +1,13 @@
+package com.haidaiban.foxlee.model;
+
+/**
+ * Created by qixiaohui on 3/12/15.
+ */
+
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class TransactionState {
+
+
+}
