dc7d4e47c90c649f5cfaac821ca235836e363703
foxlee25 <foxlee25@gmail.com>
2015-03-23 16:23:08 -0700

    keep update login and background color

---
+++ app/src/main/java/com/haidaiban/foxlee/activitys/Login_Activity.java
@@ -0,0 +1,18 @@
+package com.haidaiban.foxlee.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.haidaiban.foxlee.fragments.R;
+
+/**
+ * Created by jili on 3/23/15.
+ */
+public class Login_Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_layout);
+
+    }
+}
