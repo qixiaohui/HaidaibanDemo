7b741592c8c6c9b2eb3debcda52cc1361d6109ee
foxlee25 <foxlee25@gmail.com>
2015-03-28 16:09:06 -0700

    add search

--- app/src/main/java/com/haidaiban/foxlee/activitys/LimitedTimeOffer.java
+++ app/src/main/java/com/haidaiban/foxlee/activitys/LimitedTimeOffer.java
@@ -1,8 +1,13 @@
 package com.haidaiban.foxlee.activitys;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.haidaiban.foxlee.fragments.LimitedDealFragment;
 import com.haidaiban.foxlee.fragments.R;
@@ -13,11 +18,12 @@ import com.haidaiban.foxlee.fragments.R;
 public class LimitedTimeOffer extends ActionBarActivity{
     private FragmentTabHost tabHost;
     private String [] titles;
-
+    private ActionBar actonBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.limitedoffer);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         titles = getResources().getStringArray(R.array.tabTitle);
         tabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
@@ -30,6 +36,29 @@ public class LimitedTimeOffer extends ActionBarActivity{
 
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                openSearch();
+                return true;
 
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
+    private void openSearch() {
+        Intent intent = new Intent();
+        intent.setClass(getApplicationContext(),Search_Activity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/haidaiban/foxlee/activitys/Search_Activity.java
@@ -0,0 +1,17 @@
+package com.haidaiban.foxlee.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.haidaiban.foxlee.fragments.R;
+
+/**
+ * Created by jili on 3/28/15.
+ */
+public class Search_Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_layout);
+    }
+}
