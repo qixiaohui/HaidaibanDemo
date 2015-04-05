2c7599c49578b11f25dd0d3c5685123d7499a285
qixiaohui <qixiaohuihaha@gmail.com>
2015-04-05 16:47:11 -0500

    add order pojo schema

---
+++ app/src/main/java/com/haidaiban/foxlee/model/order/Deal.java
@@ -0,0 +1,73 @@
+package com.haidaiban.foxlee.model.order;
+
+/**
+ * Created by qixiaohui on 4/5/15.
+ */
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+
+@Generated("org.jsonschema2pojo")
+public class Deal {
+
+    @Expose
+    private String uid;
+    @Expose
+    private String title;
+    @Expose
+    private String image;
+
+    /**
+     *
+     * @return
+     * The uid
+     */
+    public String getUid() {
+        return uid;
+    }
+
+    /**
+     *
+     * @param uid
+     * The uid
+     */
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    /**
+     *
+     * @return
+     * The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     *
+     * @param title
+     * The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     *
+     * @return
+     * The image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     *
+     * @param image
+     * The image
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
