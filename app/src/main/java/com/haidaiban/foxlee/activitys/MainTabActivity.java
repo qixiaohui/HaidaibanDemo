8f60129659cd1d79b1ce1d516cd81cc15e35d7e8
foxlee25 <foxlee25@gmail.com>
2015-04-04 19:08:22 -0700

    replacement menu icon

--- app/src/main/java/com/haidaiban/foxlee/activitys/MainTabActivity.java
+++ app/src/main/java/com/haidaiban/foxlee/activitys/MainTabActivity.java
@@ -7,12 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TabHost.TabSpec;
-import android.widget.TextView;
 
 import com.haidaiban.foxlee.fragments.C_SentedOfferFragment;
+import com.haidaiban.foxlee.fragments.D0_OrderFragment;
 import com.haidaiban.foxlee.fragments.FragmentPage1;
 import com.haidaiban.foxlee.fragments.FragmentPage2;
-import com.haidaiban.foxlee.fragments.FragmentPage4;
 import com.haidaiban.foxlee.fragments.FragmentPage5;
 import com.haidaiban.foxlee.fragments.R;
 
@@ -25,14 +24,15 @@ public class MainTabActivity extends FragmentActivity{
 	private LayoutInflater layoutInflater;
 		
 	// each fragment
-	private Class fragmentArray[] = {FragmentPage1.class,FragmentPage2.class,C_SentedOfferFragment.class,FragmentPage4.class,FragmentPage5.class};
+	private Class fragmentArray[] = {FragmentPage1.class,FragmentPage2.class,C_SentedOfferFragment.class,D0_OrderFragment.class,FragmentPage5.class};
 	
 	//底部菜单 图片
-	private int mImageViewArray[] = {R.drawable.main_bottom_tab_category_normal,R.drawable.tab_message_btn,R.drawable.tab_selfinfo_btn,
-									 R.drawable.tab_square_btn,R.drawable.tab_more_btn};
+	private int mImageViewArray[] = {R.drawable.tab_home_btn,R.drawable.tab_a1_btn,R.drawable.tab_c2_btn,
+									 R.drawable.tab_d3_btn,R.drawable.tab_d4_btn};
 	
+	//private String mTextviewArray[] = {"菜单", "待发询价", "已发询价", "订单", "帮助"};
 	private String mTextviewArray[] = {"菜单", "待发询价", "已发询价", "订单", "帮助"};
-	
+
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_tab_layout);
@@ -53,7 +53,7 @@ public class MainTabActivity extends FragmentActivity{
 		for(int i = 0; i < count; i++){	
 			TabSpec tabSpec = mTabHost.newTabSpec(mTextviewArray[i]).setIndicator(getTabItemView(i));
 			mTabHost.addTab(tabSpec, fragmentArray[i], null);
-			mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.drawable.selector_tab_background);
+			mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.color.white);
 		}
 	}
 
@@ -63,8 +63,8 @@ public class MainTabActivity extends FragmentActivity{
 		ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
 		imageView.setImageResource(mImageViewArray[index]);
 		
-		TextView textView = (TextView) view.findViewById(R.id.textview);		
-		textView.setText(mTextviewArray[index]);
+		//TextView textView = (TextView) view.findViewById(R.id.textview);
+		//textView.setText(mTextviewArray[index]);
 	
 		return view;
 	}
---
+++ app/src/main/java/com/haidaiban/foxlee/fragments/D0_OrderFragment.java
@@ -0,0 +1,64 @@
+package com.haidaiban.foxlee.fragments;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.haidaiban.foxlee.adapter.TabPagerAdapter;
+import com.haidaiban.foxlee.google.SlidingTabLayout;
+
+import java.util.List;
+import java.util.Vector;
+
+public class D0_OrderFragment extends Fragment{
+
+    private String title;
+    private View view;
+    private ViewPager viewPager;
+    private TabPagerAdapter tabPagerAdapter;
+    private String[] categorys;
+    private List<Fragment> fragments;
+    private DealList dealList;
+    private ActionBar actionBar;
+    private SlidingTabLayout slidingTabLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view  = inflater.inflate(R.layout.d0offer_fragment,null);
+        viewPager = (ViewPager) view.findViewById(R.id.pager);
+        slidingTabLayout = (SlidingTabLayout) view.findViewById(R.id.sliding_tabs);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        categorys = getActivity().getResources().getStringArray(R.array.OrderTitle);
+        fragments = new Vector<Fragment>();
+        for(String category: categorys){
+            dealList = new DealList();
+            dealList.setTitle(title,category);
+            fragments.add(dealList);
+        }
+//        getSupportFragmentManager()
+        tabPagerAdapter = new TabPagerAdapter(getChildFragmentManager(),fragments,categorys);
+        viewPager.setAdapter(tabPagerAdapter);
+        viewPager.setOffscreenPageLimit(10);
+        // Center the tabs in the layout
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setViewPager(viewPager);
+
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getTitle(){
+        return title;
+    }
+}
