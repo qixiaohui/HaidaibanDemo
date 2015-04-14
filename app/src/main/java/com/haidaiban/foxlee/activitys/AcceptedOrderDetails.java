4b624159e7c23ebb50020457fa68b9416d43edce
foxlee25 <foxlee25@gmail.com>
2015-04-14 01:19:18 -0700

    add more ui on details

--- app/src/main/java/com/haidaiban/foxlee/activitys/AcceptedOrderDetails.java
+++ app/src/main/java/com/haidaiban/foxlee/activitys/AcceptedOrderDetails.java
@@ -2,6 +2,7 @@ package com.haidaiban.foxlee.activitys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.haidaiban.foxlee.Util.DataHolder;
 import com.haidaiban.foxlee.fragments.R;
@@ -11,11 +12,32 @@ import com.haidaiban.foxlee.model.offer.Result;
  * Created by tom on 4/13/15.
  */
 public class AcceptedOrderDetails extends Activity{
+
+    private TextView tx_persion_name;
+    private TextView tx_persion_adds;
+    private TextView tx_persion_phone;
+
+
     private Result acceptedOffer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.d0offer_details);
         acceptedOffer = DataHolder.getAcceptedOffer();
+
+        initView();
+        setData();
+    }
+
+    private void setData() {
+
+        tx_persion_name.setText(acceptedOffer.getCustomer().getUsername());
+    }
+
+    private void initView() {
+        tx_persion_adds  = (TextView) findViewById(R.id.tx_d1_consignee_add);
+        tx_persion_name  = (TextView) findViewById(R.id.tx_d1_consignee_name);
+        tx_persion_phone  = (TextView) findViewById(R.id.tx_d1_consignee_phone);
+
     }
 }
