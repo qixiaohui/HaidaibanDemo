6bc2c3a476d544dd3684f1e03e79dc45dddd1b38
foxlee25 <foxlee25@gmail.com>
2015-04-25 15:47:26 -0700

    add money ui

---
+++ app/src/main/java/com/haidaiban/foxlee/Base/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.haidaiban.foxlee.Base;
+
+/**
+ * Created by jili on 4/25/15.
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/haidaiban/foxlee/activitys/E1_MyMoney_Activity.java
@@ -0,0 +1,7 @@
+package com.haidaiban.foxlee.activitys;
+
+/**
+ * Created by jili on 4/25/15.
+ */
+public class E1_MyMoney_Activity {
+}
