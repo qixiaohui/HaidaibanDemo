d1f94a182ae52acc839b8dd746cc899503c4327e
foxlee25 <foxlee25@gmail.com>
2015-05-10 10:53:54 -0700

    address xml

---
+++ app/src/main/java/com/haidaiban/foxlee/activitys/E1_MyAddress_Activity.java
@@ -0,0 +1,20 @@
+package com.haidaiban.foxlee.activitys;
+
+import com.haidaiban.foxlee.Base.BaseActivity;
+import com.haidaiban.foxlee.fragments.R;
+
+/**
+ * Created by jili on 5/9/15.
+ */
+public class E1_MyAddress_Activity extends BaseActivity {
+
+    @Override
+    protected void initView() {setContentView(R.layout.e1_myaddress);
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
