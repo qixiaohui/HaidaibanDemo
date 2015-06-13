70ead6ce6a02d4b80583980412ea5392cec4691d
qixiaohuihaha@gmail.com <qixiaohuihaha@gmail.com>
2015-06-13 12:43:43 -0700

    update user profile page loading data

--- app/src/main/java/com/haidaiban/foxlee/fragments/FragmentPage5.java
+++ app/src/main/java/com/haidaiban/foxlee/fragments/FragmentPage5.java
@@ -45,9 +45,11 @@ public class FragmentPage5 extends Fragment{
     private static Intent intent;
 
     private FrameLayout myMoney ;
-
+    private Webmethod webmethod;
     private FrameLayout myAdds;
     private UserProfile userProfile;
+    private String userName;
+    private GetUserProfile getUserProfile;
 
 
     @Override
@@ -61,10 +63,11 @@ public class FragmentPage5 extends Fragment{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         logout = (TextView) view.findViewById(R.id.logout);
+        sharedPreferences = new SecurePreferences(getActivity().getApplicationContext());
+        userName = sharedPreferences.getString("userName","0");
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sharedPreferences = new SecurePreferences(getActivity().getApplicationContext());
                 editor = sharedPreferences.edit();
                 editor.remove("userName");
                 editor.remove("password");
@@ -100,13 +103,6 @@ public class FragmentPage5 extends Fragment{
         nickName = (TextView) getActivity().findViewById(R.id.nickName);
         icon = (ImageView) getActivity().findViewById(R.id.icon);
 
-        nickName.setText(userProfile.getNickname());
-
-        Picasso.with(getActivity().getApplicationContext())
-                .load(Constants.getLOGIN_URL()+userProfile.getIcon())
-                .centerCrop()
-                .resize(70, 70)
-                .into(icon);
 
         myMoney.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,7 +119,53 @@ public class FragmentPage5 extends Fragment{
             }
         });
 
+        //call async to get user profile
+        getUserProfile = new GetUserProfile();
+        getUserProfile.execute(userName);
 
+    }
+
+    public void loadData(){
+
+        if(userProfile.getNickname()!=null) {
+            nickName.setText(userProfile.getNickname());
+        }else{
+            nickName.setText(userProfile.getUser().getUsername());
+        }
+
+        if(userProfile.getIcon()!=null)
+        Picasso.with(getActivity().getApplicationContext())
+                .load(Constants.getLOGIN_URL()+userProfile.getIcon())
+                .centerCrop()
+                .resize(70, 70)
+                .into(icon);
+    }
+
+    public class GetUserProfile extends AsyncTask<String,String,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            webmethod = new Webmethod(getActivity().getApplicationContext());
+            try {
+                userProfile = webmethod.isAgent(params[0]);
+            }catch (IOException e){
+                Toast.makeText(getActivity().getApplicationContext(),"can't load user profile, try later",Toast.LENGTH_LONG).show();
+            }catch (JSONException e){
+                Toast.makeText(getActivity().getApplicationContext(),"fail to parse user profile",Toast.LENGTH_LONG).show();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            loadData();
+        }
     }
 
 }
