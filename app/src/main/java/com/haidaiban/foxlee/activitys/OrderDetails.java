c2e4c4ecf980c7acd9a8a6dfa72c7e0a3de076cb
qixiaohuihaha@gmail.com <qixiaohuihaha@gmail.com>
2015-06-27 15:20:42 -0700

    add detail page

--- app/src/main/java/com/haidaiban/foxlee/activitys/OrderDetails.java
+++ app/src/main/java/com/haidaiban/foxlee/activitys/OrderDetails.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.haidaiban.foxlee.Util.DataHolder;
+import com.haidaiban.foxlee.Util.Utility;
+import com.haidaiban.foxlee.adapter.BasicAdapter;
 import com.haidaiban.foxlee.config.Constants;
 import com.haidaiban.foxlee.fragments.R;
 import com.haidaiban.foxlee.model.order.Result;
 import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
 
 /**
  * Created by jili on 4/7/15.
@@ -26,8 +31,9 @@ public class OrderDetails extends Activity {
     TextView updateTimer;
     ImageView orderImage;
     TextView orderStatus;
-    Button terminateOrder;
-    Result order;
+    private static Result order;
+    private static String time;
+    private static ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,32 +43,61 @@ public class OrderDetails extends Activity {
         updateTimer = (TextView) findViewById(R.id.textView_updatetimer);
         orderImage = (ImageView) findViewById(R.id.c0_img);
         orderStatus = (TextView) findViewById(R.id.orderStatus);
-        terminateOrder = (Button) findViewById(R.id.terminateOrder);
+        listView = (ListView) findViewById(R.id.list);
 
         loadData();
     }
 
     public void loadData(){
         order = DataHolder.getOrderResult();
-        if(order.getIsSystemClosed()) {
-            tempOrder.setText(getResources().getString(R.string.c_orderdetails_closed));
-            tempOrder.setBackgroundColor(getResources().getColor(R.color.grey));
-            orderStatus.setText(getResources().getString(R.string.orderClosed));
-        }else{
-            if(order.getOpenOffers().size()>0){
-                tempOrder.setText(getResources().getString(R.string.c_orderdetails_accepted));
-                tempOrder.setBackgroundColor(getResources().getColor(R.color.green));
-                orderStatus.setText(getResources().getString(R.string.orderAccepted_1)
-                        +order.getOpenOffers().get(0).getAgent()
-                        +getResources().getString(R.string.orderAccepted_2));
-            }else {
-                tempOrder.setText(getResources().getString(R.string.c_orderdetails_waiting));
-                tempOrder.setBackgroundColor(getResources().getColor(R.color.blue));
-                orderStatus.setText(getResources().getString(R.string.c_orderdetails_18hours_1)
-                        +getResources().getString(R.string.c_orderdetails_18hours_2));
+
+        try {
+            time = Utility.getTimeSpan(order.getLastUpdate());
+            if(!time.equals("")){
+                updateTimer.setText("更新于"+time+"前");
             }
+        }catch (ParseException e) {
+            System.out.println("parse time exception***");
         }
-        orderNumber.setText(order.getQuotes().get(0).getOrder());
+
+        if(order.getIsOpen()&&order.getOpenOffers().size()>0){
+            tempOrder.setText("已报价");
+            tempOrder.setTextColor(getApplicationContext().getResources().getColor(R.color.white));
+            tempOrder.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.grey));
+            orderNumber.setText("询价单号: "+order.getUid());
+        }else if(order.getIsOpen()&&order.getOpenOffers().size()==0){
+            tempOrder.setText("待报价");
+            tempOrder.setTextColor(getApplicationContext().getResources().getColor(R.color.white));
+            tempOrder.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.blue));
+            orderNumber.setText("询价单号: "+order.getUid());
+        }else {
+            tempOrder.setText("已关闭");
+            tempOrder.setTextColor(getApplicationContext().getResources().getColor(R.color.white));
+            tempOrder.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.green));
+            orderNumber.setText("询价单号: "+order.getUid());
+            orderStatus.setText("超时无代办接单，系统已终止询价。");
+        }
+
+        listView.setAdapter(new BasicAdapter(getApplicationContext(),order));
+//
+//        if(order.getIsSystemClosed()) {
+//            tempOrder.setText(getResources().getString(R.string.c_orderdetails_closed));
+//            tempOrder.setBackgroundColor(getResources().getColor(R.color.grey));
+//            orderStatus.setText(getResources().getString(R.string.orderClosed));
+//        }else{
+//            if(order.getOpenOffers().size()>0){
+//                tempOrder.setText(getResources().getString(R.string.c_orderdetails_accepted));
+//                tempOrder.setBackgroundColor(getResources().getColor(R.color.green));
+//                orderStatus.setText(getResources().getString(R.string.orderAccepted_1)
+//                        +order.getOpenOffers().get(0).getAgent()
+//                        +getResources().getString(R.string.orderAccepted_2));
+//            }else {
+//                tempOrder.setText(getResources().getString(R.string.c_orderdetails_waiting));
+//                tempOrder.setBackgroundColor(getResources().getColor(R.color.blue));
+//                orderStatus.setText(getResources().getString(R.string.c_orderdetails_18hours_1)
+//                        +getResources().getString(R.string.c_orderdetails_18hours_2));
+//            }
+//        }
         if(order.getQuotes().get(0).getDeal()==null){
             Picasso.with(this)
                     .load(R.drawable.productnoimage)
---
+++ app/src/main/java/com/haidaiban/foxlee/adapter/BasicAdapter.java
@@ -0,0 +1,69 @@
+package com.haidaiban.foxlee.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.haidaiban.foxlee.fragments.R;
+import com.haidaiban.foxlee.model.offer.Offer;
+import com.haidaiban.foxlee.model.order.Result;
+
+/**
+ * Created by tom on 6/27/15.
+ */
+public class BasicAdapter extends BaseAdapter {
+    private static Context context;
+    private static Result orders;
+    private static LayoutInflater layoutInflater;
+    private static View view;
+
+    @Override
+    public int getCount() {
+        return orders.getQuotes().size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        view = convertView;
+        if(convertView == null){
+            view = layoutInflater.inflate(R.layout.basic_order_list_row,null);
+        }
+        ViewHolder viewHolder = null;
+        if(viewHolder == null){
+            viewHolder = new ViewHolder();
+            viewHolder.quote = (TextView) view.findViewById(R.id.quote);
+            view.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.quote.setText(orders.getQuotes().get(position).getTitle());
+        return view;
+    }
+
+    public BasicAdapter(Context context, Result orders) {
+        this.context = context;
+        this.orders = orders;
+        this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public static class ViewHolder{
+        TextView quote;
+    }
+}
